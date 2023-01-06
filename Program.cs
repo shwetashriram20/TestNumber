650701a5dac309ee6acda8b29156ad370f9756d9
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-06 18:39:45 +0530

    Test

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find the Max Num of 3 Variables Program ");
+            int X = 10;
+            int Y = 30;
+            int Z = 60;
+
+            int val = FindMaximum<int>.MaxIntNumber(X, Y, Z);
+            Console.WriteLine("The greater num is " + val);
         }
     }
+    
 }
