1a15f6279bb19fa05ccd1b4c263e043e721a0922
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-06 18:45:18 +0530

    test

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find the Max Num of 3 Variables Program ");
+            float X = 5.0F;
+            float Y = 18.3F;
+            float Z = 10.1F;
+
+            float val = FindMaximum<float>.MaxFloatNumber(X, Y, Z);
+            Console.WriteLine("The greater num is " + val);
         }
     }
 }
