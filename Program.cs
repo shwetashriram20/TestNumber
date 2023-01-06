3a7710c2ec57891c7cb846c98e7fe92c4761fe0a
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-06 18:31:52 +0530

    Greeting Massage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find the Max Num of 3 Variables Program ");
+        }
+    }
+}
