d5502aa6b61dc031ecb82bdb9308542c4419fb74
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-06 20:33:13 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find the Max Num of 3 Variables Program ");
+            int num = FindMaximum<int>.MaxNumber(75, 30, 22);
+            Console.WriteLine("The greater Integer number is : " + num);
+
         }
     }
 }
