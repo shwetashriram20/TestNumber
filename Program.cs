83b0e7ff54a8252de14cc177c6a667f051a722e4
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-06 18:49:38 +0530

    method Calling

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find the Max Num of 3 Variables Program ");
+            string X = "Apple";
+            string Y = "Peach";
+            string Z = "Banana";
+
+            string val = FindMaximum<string>.MaxOfString(X, Y, Z);
+            Console.WriteLine("The greater String is " + val);
         }
     }
 }
+
+
