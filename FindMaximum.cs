e21d28740ba6d12f9d0c322a39f21e513aed5eb3
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-09 09:48:43 +0530

    Extend Maximum

---
+++ FindMaximum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNumber
+{
+    public class FindMaximum<T> where T : IComparable
+    {
+        public T A, B, C, Max;
+        public FindMaximum(T A, T B, T C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        public static T MaxNum(T A, T B, T C)
+        {
+            if (A.CompareTo(B) > 0 && A.CompareTo(C) > 0 ||
+                    A.CompareTo(B) >= 0 && A.CompareTo(C) > 0 ||
+                    A.CompareTo(B) > 0 && A.CompareTo(C) >= 0)
+            {
+                return A;
+            }
+
+            if (B.CompareTo(A) > 0 && B.CompareTo(C) > 0 ||
+                    B.CompareTo(A) >= 0 && B.CompareTo(C) > 0 ||
+                    B.CompareTo(B) > 0 && B.CompareTo(C) >= 0)
+            {
+                return B;
+            }
+
+            if (C.CompareTo(B) > 0 && C.CompareTo(A) > 0 ||
+                    C.CompareTo(B) >= 0 && C.CompareTo(A) > 0 ||
+                    C.CompareTo(B) > 0 && C.CompareTo(A) >= 0)
+            {
+                return C;
+            }
+            return default;
+        }
+        public T MaxMethod()
+        {
+            Max = FindMaximum<T>.MaxNum(this.A, this.B, this.C);
+            toPrint(Max);
+            return Max;
+
+        }
+        public void toPrint(T Max)
+        {
+            Console.WriteLine("Maximum is " + Max);
+        }
+
+
+
+    }
+}
